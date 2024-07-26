e010bcd368390e8fceb2cb7c69d5d03f79d2214a
mei <mei28aquarius@gmail.com>
2024-07-26 10:31:12 +0900

    fix: add space after 's' in command line check
    This commit modifies the check for the 's' command in the command line.
    Previously, the check was for 's', but this could lead to false positives
    as any command starting with 's' would pass the check. By adding a space
    after the 's', we ensure that only the 's' command is matched. This
    improves the accuracy of the command detection and prevents unintended
    behaviour.

--- lua/instant_rename/init.lua
+++ lua/instant_rename/init.lua
@@ -3,7 +3,7 @@ local function s_abbreviation()
   local cmd_type = vim.fn.getcmdtype()
   local cmd_line = vim.fn.getcmdline()
 
-  if cmd_type == ":" and cmd_line == "s" then
+  if cmd_type == ":" and cmd_line == "s " then
     -- Clear the current command line with <C-u> and replace it
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-u>%s///g<Left><Left>", true, true, true), "n", false)
   end
